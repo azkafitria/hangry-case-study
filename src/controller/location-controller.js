dcb344504a70e289e772467e0f111889baac053a
Azka Fitria <azkafitriaaf@gmail.com>
2023-11-10 10:18:17 +0700

    add name in location response

--- src/controller/location-controller.js
+++ src/controller/location-controller.js
@@ -1,8 +1,8 @@
 import { getLocationService } from "../service/location-service.js";
 
-const getLocationController = (req, res, next) => {
+const getLocationController = async (req, res, next) => {
   try {
-    const result = getLocationService(req.ipInfo.loc, req.ipInfo.ip);
+    const result = await getLocationService(req.ipInfo.loc, req.ipInfo.ip);
     res.status(200).json({
       data: result,
     });
--- src/service/location-service.js
+++ src/service/location-service.js
@@ -1,17 +1,25 @@
-const getLocationService = (loc, ip) => {
+import axios from "axios";
+import { ResponseError } from "../error/response-error.js";
+
+const getLocationService = async (loc, ip) => {
   const { latitude, longitude } = loc || {
     latitude: 0,
     longitude: 0,
   };
-  const clientIp = ip;
 
-  return {
-    latitude: latitude,
-    longitude: longitude,
-    clientIp: clientIp,
-    loc: loc,
-    ip: ip,
-  };
+  try {
+    const ipGeolocationResponse = await axios.get(
+      `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.IPGEOLOCATION_API_KEY}&ip=${ip}`
+    );
+
+    return {
+      latitude: latitude,
+      longitude: longitude,
+      name: `${ipGeolocationResponse.data.city}, ${ipGeolocationResponse.data.country_name}`,
+    };
+  } catch (e) {
+    throw new ResponseError(500, "Something went wrong.");
+  }
 };
 
 export { getLocationService };
